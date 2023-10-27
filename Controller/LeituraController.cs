eb5fcaa9ef8320bf4035d8dc277498e2ab082a56
Nathalia Cosim <nathalia.pcosim@gmail.com>
2023-10-27 10:42:05 -0400

    Adicionar arquivos de projeto.

---
+++ Controller/LeituraController.cs
@@ -0,0 +1,27 @@
+﻿using ClosedXML.Excel;
+
+namespace LeituraArquivo.Controller
+{
+    public class LeituraController
+    {
+        public LeituraController()
+        {
+
+        }
+
+        public async Task Ler()
+        {
+            var xls = new XLWorkbook(@"D:\STAF\CC_2024.xlsx");
+            var planilha = xls.Worksheets.First(w => w.Name == "ELENCO_TCE_2024");
+            var totalLinhas = planilha.Rows().Count();
+            // primeira linha é o cabecalho
+            for (int l = 2; l <= totalLinhas; l++)
+            {
+                var codigo = planilha.Cell($"I{l}").Value.ToString();
+                var descricao = planilha.Cell($"J{l}").Value.ToString();
+                var pai = planilha.Cell($"P{l}").Value.ToString();
+                Console.WriteLine($"{codigo} - {descricao} - {pai}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using LeituraArquivo.Controller;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        LeituraController leituraController = new LeituraController();
+        await leituraController.Ler();
+    }
+}
